8f20346b4668573204e3883167b797b568184100
Metasov Arthur <metasov@gmail.com>
2014-10-12 23:25:26 +0300

    Add dummy feed view

--- news/views.py
+++ news/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from news.models import News, DeletedNews, User
+from django.core.paginator import Paginator, EmptyPage
+
+PAGE_NEWS_COUNT = 5
 
 # Create your views here.
+@login_required
+def feed(request, page_num=None):
+	page_num = page_num or 1
+	news = News.objects.all()
+	p = Paginator(news, PAGE_NEWS_COUNT)
+	try:
+		page = p.page(page_num)
+	except EmptyPage:
+		page = p.page(p.num_pages)
+
+	result = "<br/>\n".join([ "%(title)s(%(description)s)" % page.object_list])
+
+	return HttpResponse(result)
--- newstest/urls.py
+++ newstest/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from news import views as news
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'newstest.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', news.feed),
     url(r'^admin/', include(admin.site.urls)),
 )
