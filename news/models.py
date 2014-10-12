d7166f2aa42453cb9c346d41d1b04add6e8e3b5b
Metasov Arthur <metasov@gmail.com>
2014-10-12 23:11:22 +0300

    add News and DeletedNews models to news app

--- news/models.py
+++ news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime, date
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class News(models.Model):
+	title = models.CharField("Title", max_length=120)
+	description = models.TextField("Description")
+	creation_date = models.DateField("Creation Date", default=date.today)
+	publication_date = models.DateField("Publication Date", default=date.today)
+	order = models.IntegerField("Order", default=1)
+	class Meta:
+		ordering = ('order', )
+
+class DeletedNews(models.Model):
+	user = models.ForeignKey(User, related_name="deleted_news")
+	news = models.ForeignKey(News, related_name="users_deleted")
