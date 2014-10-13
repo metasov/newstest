568eb711980530bbcdd9ee680ebc2429d361c0e9
Metasov Arthur <metasov@gmail.com>
2014-10-13 10:20:26 +0300

    Add __unicode__ and verbose names to models

--- news/models.py
+++ news/models.py
@@ -12,7 +12,17 @@ class News(models.Model):
 	order = models.IntegerField("Order", default=1)
 	class Meta:
 		ordering = ('order', )
+		verbose_name = "News"
+		verbose_name_plural = "News"
+
+	def __unicode__(self):
+		return self.title
 
 class DeletedNews(models.Model):
 	user = models.ForeignKey(User, related_name="deleted_news")
 	news = models.ForeignKey(News, related_name="users_deleted")
+	class Meta:
+		verbose_name = "Deleted News"
+		verbose_name_plural = "Deleted News"
+	def __unicode__(self):
+		return "%s <--> %s" % (self.user.email, self.news.title)
