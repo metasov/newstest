319564a8977c71ed64bdeff7f2a4bc7c12bc2d6f
Metasov Arthur <metasov@gmail.com>
2014-10-13 19:55:00 +0300

    Some reordering in main.js

--- news/static/js/main.js
+++ news/static/js/main.js
@@ -9,33 +9,25 @@ $(document).ready(function(){
 			});
 			return false;
 		},
-		renew_events = function() {
-			$('a[data-trigger="load_page"]').click(page_click)
-			$('a[class="remove"]').click(remove_click)
-			$("[data-toggle='confirmation']").popConfirm();			
-		};
-
-	renew_events();
-
-	var	load_page = function(page_num, success_callback) {
-		$.ajax({
-			dataType: 'json',
-			url: '/' + page_num + '/',
-			success: function( data ) {
-				window.page_num = page_num;
-				var result = $( "<ul/>", {
-				    "class": "media-list",
-				    html: data.news.join( "" )
-				})
-				$("ul.media-list").replaceWith(result);
-				result = $( "<ul/>", {
-				    "class": "pagination",
-				    html: data.pagination
-				})
-				$("ul.pagination").replaceWith(result);
-				renew_events();
-			}
-		})
+		load_page = function(page_num, success_callback) {
+			$.ajax({
+				dataType: 'json',
+				url: '/' + page_num + '/',
+				success: function( data ) {
+					window.page_num = page_num;
+					var result = $( "<ul/>", {
+					    "class": "media-list",
+					    html: data.news.join( "" )
+					})
+					$("ul.media-list").replaceWith(result);
+					result = $( "<ul/>", {
+					    "class": "pagination",
+					    html: data.pagination
+					})
+					$("ul.pagination").replaceWith(result);
+					renew_events();
+				}
+			})
 		},
 		remove = function(news_id, success_callback) {
 			$.ajax({
@@ -47,5 +39,14 @@ $(document).ready(function(){
 					}
 				}
 			})
-		}
+		},
+		renew_events = function() {
+			$('a[data-trigger="load_page"]').click(page_click)
+			$('a[class="remove"]').click(remove_click)
+			$("[data-toggle='confirmation']").popConfirm();			
+		};
+
+	renew_events();
+
+	
 })
